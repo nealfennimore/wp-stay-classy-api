d8152b5f940da8efbae0c9cf32f57c68e8870177
nealfennimore <hi@neal.codes>
2015-03-20 18:10:05 -0700

    Add account calls and campaign tickets. Better commenting throughout plugin:
    - Remove func_get_args calls
    - All opts should now take just an array and removed check for associative array when creating a url

--- classy-api/classy-api.php
+++ classy-api/classy-api.php
@@ -19,24 +19,63 @@ class Classy_API {
                  $token = CLASSY_TOKEN;
 
 // ================================================
+//                   Account
+// ================================================
+
+  /**
+   * /account-info
+   * [get_account_info Curl to classy api to account information]
+   * @return [array] [Returns account info]
+   */
+  public static function get_account_info(){
+    $data = self::api_handler('account-info');
+    return $data;
+  }
+
+  /**
+   * /account-activity
+   * [get_account_activity Curl to classy api to get all account activity]
+   * @param  [array/hash] $opts [Optional parameters]
+   * @return [array] [Returns account activity]
+   */
+  public static function get_account_activity($opts=NULL){
+    $data = self::api_handler('account-activity', $opts);
+    return $data['activity'];
+  }
+
+  /**
+   * /account-sponsor-matching
+   * [get_campaign_sponsors Curl to classy api to get information about matching sponsors for a campaign/event]
+   * @param  [int] $eid [Need an event ID]
+   * @return [array] [Returns matching sponsors for a campaign/event]
+   */
+  public static function get_campaign_sponsors($eid=NULL){
+    if (!is_int($eid)) { return self::throw_error('Need a valid event ID', __LINE__); }
+    $data = self::api_handler('account-sponsor-matching', array('eid' => $eid));
+    return $data;
+  }
+
+
+// ================================================
 //                   Campaigns
 // ================================================
 
   /**
+   * /campaigns
    * [get_campaigns Curl to classy api to get all campaigns]
-   * @param  [obj] $opts [Optional parameters for /campaigns]
-   * @return [arr of objs] [Returns all campaigns as objects]
+   * @param  [array/hash] $opts [Optional parameters]
+   * @return [array] [Returns all campaigns]
    */
   public static function get_campaigns($opts=NULL){
-    $opts = func_get_args();
     $data = self::api_handler('campaigns', $opts);
     return $data['campaigns'];
   }
 
   /**
+   * /campaign-info
    * [get_campaign Curl to classy api to get info on a campaign]
    * @param  [int] $eid [Need an event ID]
-   * @return [arr of objs] [Returns campaign info]
+   * @return [array] [Returns campaign info]
    */
   public static function get_campaign($eid=NULL){
     if (!is_int($eid)) { return self::throw_error('Need a valid event ID', __LINE__); }
@@ -44,25 +83,38 @@ class Classy_API {
     return $data;
   }
 
+  /**
+   * /campaign-tickets
+   * [get_campaign_tickets Curl to classy api to get tickets on a campaign]
+   * @param  [int] $eid [Need an event ID]
+   * @return [array] [Returns campaign tickets]
+   */
+  public static function get_campaign_tickets($eid=NULL){
+    if (!is_int($eid)) { return self::throw_error('Need a valid event ID', __LINE__); }
+    $data = self::api_handler('campaign-tickets', array('eid' => $eid));
+    return $data['tickets'];
+  }
+
 // ================================================
 //                  Fundraisers
 // ================================================
 
   /**
+   * /fundraisers
    * [get_fundraisers Curl to classy api to get all fundraisers]
-   * @param  [obj] $opts [Optional parameters for /fundraisers]
-   * @return [arr of objs] [Returns all fundraisers as objects]
+   * @param  [array/hash] $opts [Optional parameters]
+   * @return [array] [Returns all fundraisers as an array]
    */
   public static function get_fundraisers($opts=NULL){
-    $opts = func_get_args();
     $data = self::api_handler('fundraisers', $opts);
     return $data['fundraisers'];
   }
 
   /**
+   * /fundraiser-info
    * [get_fundraiser Curl to classy api to get info on a fundraiser]
    * @param  [int] $fcid [Need a fundraiser campaign ID]
-   * @return [arr of objs] [Returns fundraiser info]
+   * @return [array] [Returns fundraiser info]
    */
   public static function get_fundraiser($fcid=NULL){
     if (!is_int($fcid)) { return self::throw_error('Need a valid fundraising campaign ID', __LINE__); }
@@ -70,52 +122,26 @@ class Classy_API {
     return $data;
   }
 
-
-// ================================================
-//                  Donations
-// ================================================
-
-  /**
-   * [get_donations Curl to classy api to get donations]
-   * @param  [obj] $opts [Optional parameters for /donations]
-   * @return [arr of objs]       [Returns donations]
-   */
-  public static function get_donations($opts=NULL){
-    $opts = func_get_args();
-    $data = self::api_handler('donations', $opts);
-    return $data['donations'];
-  }
-
-  /**
-   * [get_recurring Curl to classy api to get recurring donations]
-   * @param  [obj] $opts [Optional parameters for /recurring]
-   * @return [arr of objs]       [Returns recurring donations]
-   */
-  public static function get_recurring($opts=NULL){
-    $opts = func_get_args();
-    $data = self::api_handler('recurring', $opts);
-    return $data['profiles'];
-  }
-
 // ================================================
 //                      Teams
 // ================================================
 
   /**
+   * /teams
    * [get_teams Curl to classy api to get teams]
-   * @param  [obj] $opts [Optional parameters for /teams]
-   * @return [arr of objs]       [Returns teams]
+   * @param  [array/hash] $opts [Optional parameters]
+   * @return [array] [Returns teams]
    */
   public static function get_teams($opts=NULL){
-    $opts = func_get_args();
     $data = self::api_handler('teams', $opts);
     return $data['teams'];
   }
 
   /**
+   * /team-info
    * [get_team Curl to classy api to get info on a team]
    * @param  [int] $ftid [Need fundraising team ID]
-   * @return [arr of objs] [Returns team info]
+   * @return [array] [Returns team info]
    */
   public static function get_team($ftid=NULL){
     if (!is_int($ftid)) { return self::throw_error('Need a valid fundraising team ID', __LINE__); }
@@ -124,13 +150,40 @@ class Classy_API {
   }
 
 // ================================================
+//                  Donations
+// ================================================
+
+  /**
+   * /donations
+   * [get_donations Curl to classy api to get donations]
+   * @param  [array/hash] $opts [Optional parameters]
+   * @return [array] [Returns donations]
+   */
+  public static function get_donations($opts=NULL){
+    $data = self::api_handler('donations', $opts);
+    return $data['donations'];
+  }
+
+  /**
+   * /recurring
+   * [get_recurring Curl to classy api to get recurring donations]
+   * @param  [array/hash] $opts [Optional parameters]
+   * @return [array] [Returns recurring donations]
+   */
+  public static function get_recurring($opts=NULL){
+    $data = self::api_handler('recurring', $opts);
+    return $data['profiles'];
+  }
+
+// ================================================
 //                    Projects
 // ================================================
 
   /**
+   * /project-info
    * [get_project Curl to classy api to get info on a project]
    * @param  [int] $pid [Need project ID]
-   * @return [arr of objs] [Returns project info]
+   * @return [array] [Returns project info]
    */
   public static function get_project($pid=NULL){
     if (!is_int($pid)) { return self::throw_error('Need a valid PID', __LINE__); }
@@ -143,27 +196,27 @@ class Classy_API {
 // ================================================
 
   /**
+   * /fb-friend-activity
    * [get_facebook Curl to classy api to get info on a facebook activity]
-   * @param  [int || array] $opts [Need facebook user ID(fb_uid) or list of friends IDs (friends_ids)]
-   * @return [arr of objs] [Returns fundraisers and/or donations for given ID]
+   * @param  [int || array/hash] $opts [Need facebook user ID(fb_uid) or list of friends IDs (friends_ids)]
+   * @return [arrays] [Returns fundraisers and/or donations for given ID]
    */
   public static function get_fb_activity($opts=NULL){
-    $opts = func_get_args();
     $data = self::api_handler('fb-friend-activity', $opts);
     return $data;
   }
 
 // ================================================
-//                     Classy
+//                   Classy API
 // ================================================
 
   /**
    * [api_handler description]
    * @param  [str] $queryType [Top level collection i.e. /recurring /fundraisers /teams]
-   * @param  [obj] $opts      [Options for the query: array('limit' => 2, 'eid' => 000000)]
-   * @return [arr of objs]    [Data from API call]
+   * @param  [array/hash] $opts [Options for the query: array('limit' => 2, 'eid' => 000000)]
+   * @return [array] [Data from API call]
    */
-  private static function api_handler($queryType, $opts){
+  private static function api_handler($queryType=NULL, $opts=NULL){
     $url = self::create_url($queryType, $opts);
     $data = self::fetch_data($url);
     return $data;
@@ -172,7 +225,7 @@ class Classy_API {
   /**
    * [create_url Creates the Classy URL]
    * @param  string $queryType [Top level collection]
-   * @param  [arr of objs || obj] $opts [Options for the query: array('limit' => 2, 'eid' => 000000)]
+   * @param  [array/hash || obj] $opts [Options for the query: array('limit' => 2, 'eid' => 000000)]
    * @return [str]             [URL]
    */
   private static function create_url($queryType='', $opts=NULL){
@@ -182,9 +235,7 @@ class Classy_API {
       return self::throw_error('Options is not an array', __LINE__);
     }
 
-    if ( isset($opts[0]) && is_array($opts[0]) && !empty($opts[0])) {
-      $url = self::$api_endpoint . $queryType . self::create_params($opts[0]);
-    } elseif (is_array($opts) && !empty($opts)) {
+    if (is_array($opts) && !empty($opts)) {
       $url = self::$api_endpoint . $queryType . self::create_params($opts);
     } else {
       $url = self::$api_endpoint . $queryType . self::create_params();
@@ -194,7 +245,7 @@ class Classy_API {
 
   /**
    * [create_params Creates params for Classy]
-   * @param  [obj] $args [params of str and val]
+   * @param  [array/hash] $args [params of str and val]
    * @return [str] [Returns concatenated params as http ready string]
    */
   private static function create_params($args=NULL){
